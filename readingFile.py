83f0cb90a70222f139f30047d317cbad2a2ee563
Elodie Aumaitre <60011569+EloloeA@users.noreply.github.com>
2020-05-06 18:37:30 +0200

    Create readingFile.py
    Création de fonction qui lit un fichier texte.
    Il me manque la fin : c'est à dire afficher le titre du film (en cours :) )

---
+++ readingFile.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  6 17:13:09 2020
+
+@author: elo
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# fonction qui lit un fichier texte
+def lectureTxt(name = 'listeFilm.txt'):
+    '''fonction qui lit un fichier texte et renvoit une liste des éléments lus'''
+    f = open(name, 'r', encoding = "utf8")
+    listeFilm = []
+    content = f.readlines()
+    for x in content:
+        listeFilm.append (x.strip())
+
+    return(listeFilm)
+    f.close() # nécessaire ?
+    
+
+# creation d'un dataframe test
+df = pd.DataFrame({'idFilms' : ['1985','1520','12','547'], 
+                   'titre' : ['Un bon film', 'un mauvais film', 'un film', 'un court métrage']}, 
+                   index = [0,1,2,3])
+
+#print(df.head())
+
+
+listeFilmLue = lectureTxt("listeFilm.txt")
+
+#TODO : rechercher la correspondance entre la listeFilmLue et la base (df ici) et afficher le titre du film.
